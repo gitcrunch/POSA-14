06d591addaf0e38107077ca39ae512407bb73e25
gitcrunch <yuanchengit@gmail.com>
2014-06-04 08:19:37 -0400

    assignment complete

--- grading-drivers/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
+++ grading-drivers/week-3-assignment-2/src/edu/vuum/mocca/SimpleSemaphore.java
@@ -16,23 +16,29 @@ public class SimpleSemaphore {
      * Define a ReentrantLock to protect the critical section.
      */
     // TODO - you fill in here
-
+	private final ReentrantLock mLock;
+	
     /**
      * Define a ConditionObject to wait while the number of
      * permits is 0.
      */
     // TODO - you fill in here
-
+	private final Condition notEmpty;
     /**
      * Define a count of the number of available permits.
      */
     // TODO - you fill in here.  Make sure that this data member will
     // ensure its values aren't cached by multiple Threads..
-
+	private int mCount;
     public SimpleSemaphore(int permits, boolean fair) {
         // TODO - you fill in here to initialize the SimpleSemaphore,
         // making sure to allow both fair and non-fair Semaphore
         // semantics.
+    	mLock = new ReentrantLock(fair);
+    	notEmpty = mLock.newCondition();
+    	mCount = permits;
+   
+    	//sync = fair ? new FairSync(permits) : new NonfairSync(permits);
     }
 
     /**
@@ -41,6 +47,15 @@ public class SimpleSemaphore {
      */
     public void acquire() throws InterruptedException {
         // TODO - you fill in here.
+    	final ReentrantLock lock = this.mLock;
+    	lock.lock();
+    	try {
+    		while (mCount == 0)
+    		notEmpty.await();
+    	}
+    	finally {
+    	mCount--;
+    	}
     }
 
     /**
@@ -49,6 +64,27 @@ public class SimpleSemaphore {
      */
     public void acquireUninterruptibly() {
         // TODO - you fill in here.
+    	final ReentrantLock lock = this.mLock;
+    	try {
+			lock.lockInterruptibly();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	try {
+    		while (mCount == 0)
+    			notEmpty.await();
+    		
+    	} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			mCount--;
+    			}
+    		
+    	
+    
+    
     }
 
     /**
@@ -56,6 +92,10 @@ public class SimpleSemaphore {
      */
     void release() {
         // TODO - you fill in here.
+    	final ReentrantLock lock = this.mLock;
+    	lock.unlock();
+    	mCount++;
+    	
     }
 
     /**
@@ -64,6 +104,6 @@ public class SimpleSemaphore {
     public int availablePermits() {
         // TODO - you fill in here by changing null to the appropriate
         // return value.
-        return null;
+        return mCount;
     }
 }
